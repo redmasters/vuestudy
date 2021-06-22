1de4087ed2d4504edce2331da987a826056b5f22
redmasters <erasmo.cjunior@gmail.com>
2021-06-22 19:04:13 -0300

    Saves

--- Essenciais/4 - Sintaxe de Templates/index.js
+++ Essenciais/4 - Sintaxe de Templates/index.js
@@ -1,7 +1,43 @@
-var app = new Vue({
+<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>
+
+
+var app = new Vue(({
     el: "#app",
     data: {
         message: 'Hello',
     }
 
+})
+
+var watch = new Vue({
+    el: '#watch',
+    data: {
+        question: '',
+        resposta: 'Digita ae',
+    },
+    watch: {
+        question: function(novaQuest, oldQuest) {
+            this.resposta = 'Esperando vc digitar....'
+            this.debouncedGetAnswer()
+        }
+    },
+    created: function() {
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    },
+    methods: {
+        getAnswer: function() {
+            if (this.question.indexOf('?') === -1) {
+                this.resposta = 'Perguntas teem interrogação.'
+                return
+            }
+            this.resposta = 'Pensando...'
+            var vm = this
+            axios.get('https://yesno.wtf/api')
+                .then(function (response) {
+                    vm.resposta = response.data.resposta === 'yes' ? 'Sim.' :
+                        response.data.resposta === 'no' ? 'Não.' : 'Talvez!'
+                })
+        }
+    }
 })
